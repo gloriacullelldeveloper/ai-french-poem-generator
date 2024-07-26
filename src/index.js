2f40253e0bfbfd7bdaf340e584156781f1016b08
gloriacullelldeveloper <gcullelldev@gmail.com>
2024-07-26 11:34:12 +0200

    Added JS API integration and AXIOS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 // When you submit the form, it appears a little poem in italian
 
-function showPoem(event) {
+function displayPoem(response) {
 
-    event.preventDefault();
+    console.log("Poem");
+    
     new Typewriter("#poem", {
-    strings: "I fiori della Primavera  sono i sogni",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
-});
+    });
+
+}
+
+function showPoem(event) {
+    
+    event.preventDefault();
+    
+    /* Import Axios, create apiUrl, apiKey, prompt and context, a call to the api generate poem */
+    
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "c71c7bae8f4otf0ad66fa62c747c8831";
+    let prompt = `Generate a Italian poem about ${instructionsInput.value}`;
+    let context = "You are a romantic poem expert and love to write short poems. Your mision is to create a four line Italian poem in basic HTML in each line add a space with </br>. When you finished the poem add a little signature 'SheCodes AI' and make the signature bold. Make sure to follow the user instructions";
+     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    /* call to the API*/
+
+    axios.get(apiUrl).then(displayPoem);
+
+   
 }
 
 
