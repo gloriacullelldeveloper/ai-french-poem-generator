4a7746dd11dc32f389295f5d94d0f00d3a3e7701
gloriacullelldeveloper <gcullelldev@gmail.com>
2024-07-30 10:10:17 +0200

    Changed font-family in CSS

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function showPoem(event) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "c71c7bae8f4otf0ad66fa62c747c8831";
     let prompt = `Generate a Italian poem about ${instructionsInput.value}`;
-    let context = "You are a romantic poem expert and love to write short poems. Your mision is to create a four line Italian poem in basic HTML in each line add a space with </br>. When you finished the poem add a little signature 'SheCodes AI' and make the signature bold. Make sure to follow the user instructions";
+    let context = "You are a romantic poem expert and love to write short poems. Your mision is to create a four line Italian poem in basic HTML in each line add a space with </br> and make it ONLY Italic. When you finished the poem add a little signature 'SheCodes AI' and make the signature bold. Make sure to follow the user instructions";
      let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     /* call to the API*/
 
